7c79f3c193de554b943df75d27b2e197d5dbc544
hommat <mateuszziomek2000@gmail.com>
2020-07-12 18:05:42 +0200

    Add DataTracker

---
+++ src/utils/DataTracker.test.ts
@@ -0,0 +1,116 @@
+import DataTracker from "./DataTracker";
+
+describe("DataTracker class", () => {
+  describe("showMin", () => {
+    test("return null when tracker is empty", () => {
+      const tracker = new DataTracker();
+      const result = tracker.showMin();
+
+      expect(result).toBeNull();
+    });
+
+    test("return first element when tracker has one element", () => {
+      const tracker = new DataTracker();
+      tracker.insert(5);
+      const result = tracker.showMin();
+
+      expect(result).toBe(5);
+    });
+
+    test("return minimal value when tracker has many elements", () => {
+      const tracker = new DataTracker();
+      const valuesToInsert: number[] = [5, 1, 7, -10, -2, 3, 14];
+      valuesToInsert.forEach((value) => tracker.insert(value));
+      const result = tracker.showMin();
+
+      expect(result).toBe(Math.min(...valuesToInsert));
+    });
+  });
+
+  describe("showMax", () => {
+    test("return null when tracker is empty", () => {
+      const tracker = new DataTracker();
+      const result = tracker.showMax();
+
+      expect(result).toBeNull();
+    });
+
+    test("return first element when tracker has one element", () => {
+      const tracker = new DataTracker();
+      tracker.insert(5);
+      const result = tracker.showMax();
+
+      expect(result).toBe(5);
+    });
+
+    test("return maximal value when tracker has many elements", () => {
+      const tracker = new DataTracker();
+      const valuesToInsert: number[] = [5, 1, 7, -10, 14, 3, -3];
+      valuesToInsert.forEach((value) => tracker.insert(value));
+      const result = tracker.showMax();
+
+      expect(result).toBe(Math.max(...valuesToInsert));
+    });
+  });
+
+  describe("showMean", () => {
+    test("return null when tracker is empty", () => {
+      const tracker = new DataTracker();
+      const result = tracker.showMean();
+
+      expect(result).toBeNull();
+    });
+
+    test("return first element when tracker has one element", () => {
+      const tracker = new DataTracker();
+      tracker.insert(5);
+      const result = tracker.showMean();
+
+      expect(result).toBe(5);
+    });
+
+    test("return mean value when tracker has many elements", () => {
+      const tracker = new DataTracker();
+      const valuesToInsert: number[] = [-4, 6, -2, 10, 5];
+      valuesToInsert.forEach((value) => tracker.insert(value));
+      const result = tracker.showMean();
+
+      expect(result).toBe(3);
+    });
+  });
+
+  describe("showMode", () => {
+    test("return null when tracker is empty", () => {
+      const tracker = new DataTracker();
+      const result = tracker.showMode();
+
+      expect(result).toBeNull();
+    });
+
+    test("return first element when tracker has one element", () => {
+      const tracker = new DataTracker();
+      tracker.insert(5);
+      const result = tracker.showMode();
+
+      expect(result).toBe(5);
+    });
+
+    test("return mode value when tracker has many elements and one mode", () => {
+      const tracker = new DataTracker();
+      const valuesToInsert: number[] = [-4, -2, 6, 6, -2, 10, -2, 5, 5];
+      valuesToInsert.forEach((value) => tracker.insert(value));
+      const result = tracker.showMode();
+
+      expect(result).toBe(-2);
+    });
+
+    test("return first mode value when tracker has many elements and two modes", () => {
+      const tracker = new DataTracker();
+      const valuesToInsert: number[] = [-4, -2, 6, 6, -2, 10, -2, 5, 5, 6];
+      valuesToInsert.forEach((value) => tracker.insert(value));
+      const result = tracker.showMode();
+
+      expect(result).toBe(-2);
+    });
+  });
+});
---
+++ src/utils/DataTracker.ts
@@ -0,0 +1,62 @@
+class DataTracker {
+  private valuesCount: number = 0;
+  private valuesSum: number = 0;
+
+  private minValue: number | null = null;
+  private maxValue: number | null = null;
+
+  private valuesOccured: { [value: number]: number } = {};
+  private valueOccuredTheMost: number | null = null;
+  private valueOccuredTheMostCount: number | null = null;
+
+  insert(value: number) {
+    this.valuesCount++;
+    this.valuesSum += value;
+
+    if (this.valuesCount > 1) this.nextInsert(value);
+    else this.firstInsert(value);
+  }
+
+  private firstInsert(value: number) {
+    this.minValue = value;
+    this.maxValue = value;
+    this.valuesOccured[value] = 1;
+    this.valueOccuredTheMost = value;
+    this.valueOccuredTheMostCount = 1;
+  }
+
+  private nextInsert(value: number) {
+    if (value > this.maxValue!) this.maxValue = value;
+    if (value < this.minValue!) this.minValue = value;
+
+    if (!this.valuesOccured[value]) this.valuesOccured[value] = 1;
+    else this.updateMode(value);
+  }
+
+  private updateMode(value: number) {
+    this.valuesOccured[value]++;
+
+    if (this.valuesOccured[value] > this.valueOccuredTheMostCount!) {
+      this.valueOccuredTheMost = value;
+      this.valueOccuredTheMostCount = this.valuesOccured[value];
+    }
+  }
+
+  showMin(): number | null {
+    return this.minValue;
+  }
+
+  showMax(): number | null {
+    return this.maxValue;
+  }
+
+  showMean(): number | null {
+    return this.valuesCount > 0 ? this.valuesSum / this.valuesCount : null;
+  }
+
+  showMode(): number | null {
+    return this.valueOccuredTheMost;
+  }
+}
+
+export default DataTracker;
